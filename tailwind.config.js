e175b58c8915e5b5aa513b6f807bbd9065b12c8d
w3web3 <w3webfor3@gmail.com>
2024-05-06 14:23:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+        colors: {
+            bgdark: '#1c232f',
+            gray: {
+                DEFAULT: '#f4f4f4',
+                light: '#f8f8f8',
+            },
+            primary: {
+                DEFAULT: '#5367ff',
+                light: '#eef0ff',
+            },
+            secondary: {
+                DEFAULT: '#0dcaf0',
+                light: '#e7fafe',
+            },
+            success: {
+                DEFAULT: '#00d09c',
+                light: '#e6fbf6',
+            },
+            danger: {
+                DEFAULT: '#ff585d',
+                light: '#ffeeef',
+            },
+            warning: {
+                DEFAULT: '#ffb61b',
+                light: '#fff3cd',
+            },
+            info: {
+                DEFAULT: '#1da1f2',
+                light: '#e8f6fe',
+            },
+        },
+    },
+},
+  plugins: [],
+}
+
